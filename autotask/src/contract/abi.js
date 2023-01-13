4af974c884afbdfc4e91900213f876d8d3eb87e8
Peter Sparacino <72105234+psparacino@users.noreply.github.com>
2023-01-12 20:39:00 -0500

    working autotask

---
+++ autotask/src/contract/abi.js
@@ -0,0 +1,7 @@
+const MolochABI = require("./abi/moloch-abi.json");
+
+const abi = {
+  MolochABI,
+};
+
+module.exports = abi;
---
+++ autotask/src/contract/index.js
@@ -0,0 +1,4 @@
+const abi = require("./abi.js");
+const address = require("./address.js");
+
+module.exports = { abi, address };
