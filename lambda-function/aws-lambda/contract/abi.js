872da539693c35826ce7c3e1aeae8cd1aa19781d
Peter Sparacino <72105234+psparacino@users.noreply.github.com>
2023-01-12 20:55:39 -0500

    removed lambda folder, added Readme

--- lambda-function/aws-lambda/contract/abi.js
+++
@@ -1,9 +0,0 @@
-const MolochABI = require("./abi/moloch-abi.json");
-const erc20ABI = require("./abi/erc20.json");
-
-const abi = {
-  erc20ABI,
-  MolochABI,
-};
-
-module.exports = abi;
--- lambda-function/aws-lambda/contract/index.js
+++
@@ -1,4 +0,0 @@
-const abi = require("./abi.js");
-const address = require("./address.js");
-
-module.exports = { abi, address };
